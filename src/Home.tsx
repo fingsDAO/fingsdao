397324b2fbdd5cd99f902d48c01fa008ff11e108
unknown <fings.nft@gmail.com>
2022-07-06 01:49:09 +0300

    FE Adjustments

--- src/Home.tsx
+++ src/Home.tsx
@@ -588,7 +588,7 @@ const Home = (props: HomeProps) => {
                 <MintContainer>
                     <DesContainer>
                         <NFT elevation={3}>
-                            <h2>Just an Experiment</h2>
+                            <h2>fingsDAO</h2>
                             <br/>
                             <div><Price
                                 label={isActive && whitelistEnabled && (whitelistTokenBalance > 0) ? (whitelistPrice + " " + priceLabel) : (price + " " + priceLabel)}/><Image
